c25137280a7d949fe8e1785ccb6269a3cfef8f06
panchillog <jamaicawater71@gmail.com>
2023-12-18 15:40:50 -0600

    feat: shows selectd and correct answers for each question

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,3 +1,43 @@
-<template><h1>Hello World</h1></template>
-<script setup></script>
+<template>
+  <div class="container w-11/12 my-8 h-screen">
+    <h1 class="text-center text-xl font-bold p-4">Results</h1>
+    <div class="question-container h-3/4 overflow-y-scroll">
+      <div
+        v-for="(q, index) in resultsArray"
+        :key="index"
+        class="card my-4 py-4 bg-violet-200"
+      >
+        <div class="question px-4 font-bold py-2">
+          #{{ index + 1 }} {{ q.question }}
+        </div>
+        <div
+          class="selected px-4 rounded-md font-semibold"
+          :class="q.correct === q.selected ? 'text-green-500' : 'text-red-500'"
+        >
+          selected: {{ q.selected }}
+        </div>
+        <div class="correct font-semibold px-4 mt-4 text-green-500 rounded-md">
+          Correct: {{ q.correct }}
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <button @click="playAgain" class="btn my-4 w-40 font-bold text-lg">
+        PLAY AGAIN
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useTriviaStore } from "../store/TriviaStore";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const store = useTriviaStore();
+const resultsArray = store.getResultsArray;
+
+const playAgain = () => {
+  store.resetResultsArray();
+  router.push("/");
+};
+</script>
 <style scoped></style>

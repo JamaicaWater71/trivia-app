e79d7aa2c10da0858c19e475d1b434a909dc8226
panchillog <jamaicawater71@gmail.com>
2023-12-18 01:26:11 -0600

    feature: progress bar that players can now keep track in what question they are in

--- src/views/TriviaQuestions.vue
+++ src/views/TriviaQuestions.vue
@@ -2,8 +2,25 @@
   <div class="trivia w-full h-full flex justify-center bg-violet-100">
     <div
       v-if="questions.length > 0"
-      class="w-11/12 height-5/6 min-height-5/6 mt-8 flex flex-col items-center justify-between rounded-md"
+      class="w-11/12 height-5/6 min-height-5/6 mt-8 flex flex-col items-center justify-start rounded-md"
     >
+      <!-- Progress bar -->
+      <div class="flex justify-between mb-1 w-full">
+        <span class="text-base font-medium text-blue-700 dark:text-white w-full"
+          >{{ currentQuestionIndex + 1 }}/{{ questions.length }}</span
+        >
+        <span class="text-sm font-medium text-blue-700 dark:text-white"
+          >45%</span
+        >
+      </div>
+      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+        <div
+          class="bg-blue-600 h-2.5 rounded-full"
+          :style="{
+            width: ((currentQuestionIndex + 1) / questions.length) * 100 + '%',
+          }"
+        ></div>
+      </div>
       <div
         class="question w-full h-32 bg-violet-600 text-white p-2 my-4 rounded-md"
       >

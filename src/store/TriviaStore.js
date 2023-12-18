cd430088f039cee2e1ab14b72e368e27b681c370
panchillog <jamaicawater71@gmail.com>
2023-12-18 11:20:09 -0600

    feat: store now has variable to track if an option has been selected before moving on to the next question

--- src/store/TriviaStore.js
+++ src/store/TriviaStore.js
@@ -5,17 +5,24 @@ export const useTriviaStore = defineStore({
   id: 'user',
   state: () => ({
     triviaQuestions: [],
+    selectedOption: null
 
   }),
   getters: {
    getQuestions(){
     return this.triviaQuestions
+   },
+   getSelectedOption(){
+    return this.selectedOption
    }
   },
   actions: {
     // you can use this like a method inside your components
     setQuestions(arr){
         this.triviaQuestions = arr;
+    },
+    setSelectedOption(option){
+      this.selectedOption = option
     }
 
   },
